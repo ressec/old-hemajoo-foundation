011a35324999ea3efdeaed8bb4cfda4e4691e999
Resse Christophe <christophe.resse@gmail.com>
2019-03-26 18:25:00 +0100

    feature(test): Added test classes for the @Bundle annotation

---
+++ hemajoo-foundation-common/src/test/java/com/hemajoo/foundation/common/test/resource/bundle/annotation/AbbaDiscography.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright(c) 2018 Hemajoo Ltd.
+ * ---------------------------------------------------------------------------
+ * This file is part of the Hemajoo's Incubation Software (HIS) project which is licensed
+ * under the Apache license version 2 and use is subject to license terms.
+ * You should have received a copy of the license with the project's artifact
+ * binaries and/or sources.
+ * 
+ * License can be consulted at http://www.apache.org/licenses/LICENSE-2.0
+ * ---------------------------------------------------------------------------
+ */
+package com.hemajoo.foundation.common.test.resource.bundle.annotation;
+
+import com.hemajoo.foundation.common.resource.bundle.annotation.Bundle;
+
+/**
+ * Test class annotated with the {@link Bundle} annotation.
+ * <hr>
+ * @author  <a href="mailto:christophe.resse@gmail.com">Resse Christophe - Hemajoo</a>
+ * @version 1.0.0
+ */
+@Bundle(file="bundle/discography/queen/discography-queen", root="queen.album.ten-first", priority=120)
+public class AbbaDiscography
+{
+	// EMPTY
+}
