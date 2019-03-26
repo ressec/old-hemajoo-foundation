f4a9df9b25b812e1d59f8cdbe70e29643a63df0e
Resse Christophe <christophe.resse@gmail.com>
2019-03-26 18:27:45 +0100

    feature(annotation): Implementation of the @BundleMethod annotation

---
+++ hemajoo-foundation-common/src/main/java/com/hemajoo/foundation/common/resource/bundle/annotation/BundleMethod.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright(c) 2018 Hemajoo Ltd.
+ * ---------------------------------------------------------------------------
+ * This file is part of the Hemajoo's Foundation project which is licensed
+ * under the Apache license version 2 and use is subject to license terms.
+ * You should have received a copy of the license with the project's artifact
+ * binaries and/or sources.
+ * 
+ * License can be consulted at http://www.apache.org/licenses/LICENSE-2.0
+ * ---------------------------------------------------------------------------
+ */
+package com.hemajoo.foundation.common.resource.bundle.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is used to associate to a method a resource bundle key.
+ * <hr>
+ * @author <a href="mailto:christophe.resse@gmail.com">Christophe Resse - Hemajoo</a>
+ * @version 1.0.0
+ */
+@Target({ java.lang.annotation.ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface BundleMethod
+{
+	/**
+	 * The key of the resource bundle entry.
+	 * <hr>
+	 * @return Key of the resource bundle entry.
+	 */
+	String key();
+}
